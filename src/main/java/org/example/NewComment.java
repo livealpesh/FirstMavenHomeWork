46c6890b54f5ede8958a3d95c867378bdd51a405
livealpesh <alpeshpan1112@gmail.com>
2022-10-31 21:53:04 +0000

    add-new-comment-homework

--- src/main/java/org/example/NewComment.java
+++ src/main/java/org/example/NewComment.java
@@ -1,2 +1,39 @@
-package org.example;public class NewComment {
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class NewComment
+{
+ public static WebDriver driver;
+
+    public static void main(String[] args)
+    {
+       System.setProperty("webdriver.chrome.driver","src/test/java/Driver/chromedriver.exe") ;
+       driver = new ChromeDriver();
+
+       driver.manage().window().maximize();
+
+       driver.get("https://demo.nopcommerce.com/");
+
+       driver.findElement(By.xpath("//div[@class=\"news-item\"]/div[3]/a[@href=\"/nopcommerce-new-release\"]")).click();
+
+       driver.findElement(By.className("enter-comment-title")).sendKeys("Alpesh Panchal");
+
+       driver.findElement(By.className("enter-comment-text")).sendKeys("Excellent Update");
+
+       driver.findElement(By.className("buttons")).click();
+
+       String regMsg = driver.findElement(By.className("result")). getText();
+
+       System.out.println("confirm massage for posting a Comment:\t " + regMsg);
+
+       driver.quit();
+
+
+    }
+
+
+
 }
